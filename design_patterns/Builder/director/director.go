f1ee04dc286bf75884b161c0354c2b9ce6c88da9
Mephis Pheies <mephistommm@gmail.com>
2019-02-26 23:23:35 +0800

    write Director of Builder design pattern

---
+++ design_patterns/Builder/director/director.go
@@ -0,0 +1,49 @@
+// Copyright © 2019 Mephis Pheies <mephistommm@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+package director
+
+import "gAaD/design_patterns/Builder/builder"
+
+type Director struct {
+	builder builder.Builder
+}
+
+func NewDirector(builder builder.Builder) *Director {
+	return &Director{
+		builder: builder,
+	}
+}
+
+// Construct  ...
+func (drctr *Director) Construct() {
+	drctr.builder.MakeTitle("Greeting")
+	drctr.builder.MakeString("From Morning To Afternoon")
+	drctr.builder.MakeItems([]string{
+		"Good Morning.",
+		"Good Afternoon.",
+	})
+	drctr.builder.MakeString("Good Evening")
+	drctr.builder.MakeItems([]string{
+		"Good Evening.",
+		"Good Night.",
+		"Good Bye.",
+	})
+	drctr.builder.Close()
+}
